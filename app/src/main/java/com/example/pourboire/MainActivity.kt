95fa97b00d19630e4ebccae5fcecaa452ac0cd45
chris-mayamba <christianmayamba04@gmail.com>
2025-01-23 12:19:05 +0200

    ajout de la fonction EditNUmberField

--- app/src/main/java/com/example/pourboire/MainActivity.kt
+++ app/src/main/java/com/example/pourboire/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.pourboire
 
 import android.os.Bundle
+import androidx.compose.runtime.mutableStateOf
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
@@ -17,6 +19,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -34,7 +37,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PourboireTheme {
-                    TipTimeLayout()
+                TipTimeLayout()
             }
         }
     }
@@ -57,6 +60,7 @@ fun TipTimeLayout() {
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
         )
+        EditNumberField(modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
         Text(
             text = stringResource(R.string.tip_amount, "$0.00"),
             style = MaterialTheme.typography.displaySmall
@@ -68,6 +72,16 @@ fun TipTimeLayout() {
 private fun calculateTip(amount : Double, tipPercent: Double = 15.0) : String{
     val tip = tipPercent / 100 * amount
     return NumberFormat.getCurrencyInstance().format(tip)
+}
+
+@Composable
+fun EditNumberField(modifier: Modifier = Modifier) {
+    var amountInput = mutableStateOf("0")
+    TextField(
+        value = amountInput.value,
+        onValueChange = { amountInput.value = it },
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
